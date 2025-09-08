5b4f2c93f31a4a03f80452c27dda0bd6835a49b8
adenamar03 <adenn.amar@gmail.com>
2025-09-09 02:52:15 +0500

    chore: removed venv from tracking and added .gitignore

--- ai_agent.py
+++ ai_agent.py
@@ -1,3 +1,10 @@
-# step # 1 --> set up api keys for groq and tavily
+# step # 1 --> set up api keys for groq, openai and tavily
+
+import os
+
+GROQ_API_KEY=os.environ.get("GROQ_API_KEY")
+TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
+OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+
 #step # 2 --> setup llm and the tools
 #step # 3 --> setup AI Agent with Search tool functionality
